11cc17f721d849fe2f1484dca10e9849124c4e5a
Alwaysbesideyou <62324232+Alwaysbesideyou@users.noreply.github.com>
2024-01-02 19:39:32 +0800

    Update cn.ts
    Tips

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -359,7 +359,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "有什么可以帮你的吗？\n若未反应，尝试切换模型为google-gemini-pro",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
