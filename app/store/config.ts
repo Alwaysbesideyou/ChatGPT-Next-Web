7f6ae8f98b333b4ca31115517b457563f2752560
Alwaysbesideyou <62324232+Alwaysbesideyou@users.noreply.github.com>
2024-01-02 19:35:56 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -46,7 +46,7 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gemini-pro" as ModelType,
+    model: "google-gemini-pro" as ModelType,
     temperature: 0.5,
     top_p: 1,
     max_tokens: 4000,
